afe1ec26aa8d08655284a638e13800b364835e57
Nicholas Benson <nbenson@leapmotion.com>
2016-08-22 15:30:55 -0700

    Added licenses.

--- Assets/InteractionEngine101/Scripts/Util/PhysicalButton.cs
+++ Assets/InteractionEngine101/Scripts/Util/PhysicalButton.cs
@@ -1,4 +1,12 @@
-﻿using Leap.Unity;
+﻿/******************************************************************************\
+* Copyright (C) 2012-2016 Leap Motion, Inc. All rights reserved.               *
+* Leap Motion proprietary and confidential. Not for distribution.              *
+* Use subject to the terms of the Leap Motion SDK Agreement available at       *
+* https://developer.leapmotion.com/sdk_agreement, or another agreement         *
+* between Leap Motion and you, your company or other organization.             *
+\******************************************************************************/
+
+using Leap.Unity;
 using Leap.Unity.Interaction;
 using UnityEngine;
 using UnityEngine.Events;
